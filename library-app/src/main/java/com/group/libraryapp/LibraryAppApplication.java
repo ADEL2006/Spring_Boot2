2421cd0a0c52b83aaeec97db55674b732b61b9d0
양원준 <anyas1398@gmail.com>
2023-08-27 23:51:21 +0900

    ,

--- library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryAppApplication {
+  // my second Spring practice
 
   public static void main(String[] args) {
     SpringApplication.run(LibraryAppApplication.class, args);
