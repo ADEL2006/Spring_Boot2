bf3f71a65d1848df10faabef8e34b189deef82fa
양원준 <anyas1398@gmail.com>
2023-08-28 22:12:39 +0900

    Update LibraryAppApplication.java

--- library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryAppApplication {
   // my second Spring boot practice
+  // review and use
 
   public static void main(String[] args) {
     SpringApplication.run(LibraryAppApplication.class, args);
