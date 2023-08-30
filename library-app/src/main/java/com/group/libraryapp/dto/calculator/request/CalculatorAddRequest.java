07809309d1cd2999919ebccc82b92b100e625c26
양원준 <anyas1398@gmail.com>
2023-08-31 08:27:30 +0900

    .

--- library-app/src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorAddRequest.java
+++ library-app/src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorAddRequest.java
@@ -8,4 +8,12 @@ public class CalculatorAddRequest {
         this.number1 = number1;
         this.number2 = number2;
     }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
 }
