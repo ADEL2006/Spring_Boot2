4b6a193b8c001d8dcc0eeccfcad73e98f8b97902
양원준 <anyas1398@gmail.com>
2023-08-31 23:50:48 +0900

    Update UserController.java

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,13 +1,16 @@
 package com.group.libraryapp.controller.user;
 
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     @PostMapping("/user") // POST /user
-    public void saveUser() {
-
+    public void saveUser(@RequestBody UserCreateRequest request) {
+        
     }
 }
