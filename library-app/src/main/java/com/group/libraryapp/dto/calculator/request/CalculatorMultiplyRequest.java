bced443dd9cf9ce5bbb767a2fcf5f3563ad636f1
양원준 <anyas1398@gmail.com>
2023-08-31 23:18:35 +0900

    .

---
+++ library-app/src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorMultiplyRequest.java
@@ -0,0 +1,14 @@
+package com.group.libraryapp.dto.calculator.request;
+
+public class CalculatorMultiplyRequest {
+    private int Number1;
+    private int Number2;
+
+    public int getNumber1() {
+        return Number1;
+    }
+
+    public int getNumber2() {
+        return Number2;
+    }
+}
