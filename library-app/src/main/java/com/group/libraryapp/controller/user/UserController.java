0c16f41216b2f139b95bfdf6a2a418fc79d08f7a
양원준 <anyas1398@gmail.com>
2023-08-31 23:50:00 +0900

    .

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,4 +1,13 @@
 package com.group.libraryapp.controller.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @PostMapping("/user") // POST /user
+    public void saveUser() {
+
+    }
 }
