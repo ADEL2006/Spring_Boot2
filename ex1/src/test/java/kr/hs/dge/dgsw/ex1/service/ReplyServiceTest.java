2d771fd7254528b070f96643dca2c383341b1010
양원준 <anyas1398@gmail.com>
2024-04-11 11:01:44 +0900

    Update ReplyServiceTest.java

--- ex1/src/test/java/kr/hs/dge/dgsw/ex1/service/ReplyServiceTest.java
+++ ex1/src/test/java/kr/hs/dge/dgsw/ex1/service/ReplyServiceTest.java
@@ -1,8 +1,18 @@
 package kr.hs.dge.dgsw.ex1.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ReplyServiceTest {
-    
+    @Autowired
+    private ReplyService replyService;
 
+    @Test
+    void testRegister() {
+        
+    }
 }
