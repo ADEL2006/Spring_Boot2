edd3fc0198af26fe5201e4afbf9a6a59e0300dd3
양원준 <anyas1398@gmail.com>
2024-04-24 14:07:25 +0900

    .

--- ex1/src/main/java/kr/hs/dge/dgsw/ex1/security/CustomUserDetailsService.java
+++ ex1/src/main/java/kr/hs/dge/dgsw/ex1/security/CustomUserDetailsService.java
@@ -1,13 +1,27 @@
 package kr.hs.dge.dgsw.ex1.security;
 
+import kr.hs.dge.dgsw.ex1.entity.MemberEntity;
+import kr.hs.dge.dgsw.ex1.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        MemberEntity memberEntity = memberRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+//        if(result.isEmpty()){
+//            throw new UsernameNotFoundException("User not found with username: " + username);
+//        }
         return null;
     }
 }
