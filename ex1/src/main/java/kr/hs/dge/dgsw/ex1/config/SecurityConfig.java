6796a7880dd2a7705f0b40b95453505ce32a8a2a
양원준 <anyas1398@gmail.com>
2024-05-02 10:10:17 +0900

    Update SecurityConfig.java

--- ex1/src/main/java/kr/hs/dge/dgsw/ex1/config/SecurityConfig.java
+++ ex1/src/main/java/kr/hs/dge/dgsw/ex1/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package kr.hs.dge.dgsw.ex1.config;
 
 import kr.hs.dge.dgsw.ex1.jwt.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,6 +31,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        return null;
+        return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    
 }
