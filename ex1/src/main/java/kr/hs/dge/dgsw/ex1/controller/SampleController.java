84505938d763d109bc0f5457e3f809bc6f851d26
양원준 <anyas1398@gmail.com>
2024-05-22 14:39:48 +0900

    Update SampleController.java

--- ex1/src/main/java/kr/hs/dge/dgsw/ex1/controller/SampleController.java
+++ ex1/src/main/java/kr/hs/dge/dgsw/ex1/controller/SampleController.java
@@ -1,12 +1,16 @@
 package kr.hs.dge.dgsw.ex1.controller;
 
 import kr.hs.dge.dgsw.ex1.dto.Member;
+import kr.hs.dge.dgsw.ex1.security.CustomUserDetails;
 import kr.hs.dge.dgsw.ex1.security.MemberSecurity;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +24,16 @@ public class SampleController {
     public void test1() {
         Member member = memberSecurity.getMember();
         log.info("Member: {}", member);
+    }
+
+    @GetMapping("/test2")
+    public void test2(Principal principal) {
+        log.info(".................... username: {}", principal.getName());
+    }
+
+    @GetMapping("/test3")
+    public void test3(Authentication authentication) {
+        Member member = ((CustomUserDetails) authentication.getPrincipal()).getMember();
+        log.info("................. Member: ", member);
     }
 }
