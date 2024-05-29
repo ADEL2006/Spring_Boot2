fca298069e5a012a5baa7d5b957651d7c1c5f7df
양원준 <anyas1398@gmail.com>
2024-05-29 14:01:42 +0900

    .

--- ex1/src/main/java/kr/hs/dge/dgsw/ex1/controller/UploadController.java
+++ ex1/src/main/java/kr/hs/dge/dgsw/ex1/controller/UploadController.java
@@ -1,8 +1,11 @@
 package kr.hs.dge.dgsw.ex1.controller;
 
+import kr.hs.dge.dgsw.ex1.dto.UploadResultDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,12 +29,13 @@ public class UploadController {
     private String uploadPath;
 
     @PostMapping("/upload")
-    public void uploadFile(@RequestParam("uploadFiles")List<MultipartFile> uploadFiles) {
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(@RequestParam("uploadFiles")List<MultipartFile> uploadFiles) {
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
         for (MultipartFile uploadFile: uploadFiles) {
             // image/jpg, image/jpeg
             if (!uploadFile.getContentType().startsWith("image")) {
                 log.warn("This file is not image type");
-                return;
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
             }
 
             String originalName = uploadFile.getOriginalFilename();
@@ -52,10 +57,14 @@ public class UploadController {
 
             try {
                 uploadFile.transferTo(path);
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
 
             }
         }
+        return ResponseEntity.ok(
+                resultDTOList
+        );
     }
 
     private String makeFolder() {
---
+++ ex1/src/main/java/kr/hs/dge/dgsw/ex1/dto/UploadResultDTO.java
@@ -0,0 +1,27 @@
+package kr.hs.dge.dgsw.ex1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UploadResultDTO {
+    private String fileName;
+    private String uuid;
+    private String folderPath;
+
+    public String getImageURL() {
+        try {
+            // option + command + T
+            URLEncoder.encode(folderPath + "/" + uuid + "_" + fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+}
